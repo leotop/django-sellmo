774846af104ae00dced2ea84eb6241d8f9e3cc84
Raymond Reggers <raymond@adaptiv.nl>
2012-12-07 03:19:26 +0100

    Fixed setup script to include templates, etc.

--- setup.py
+++ setup.py
@@ -26,7 +26,41 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from setuptools import setup, find_packages
+import os.path
+from setuptools import setup
+
+def fullsplit(path, result=None):
+	"""
+	Split a pathname into components (the opposite of os.path.join) in a
+	platform-neutral way.
+	"""
+	if result is None:
+		result = []
+	head, tail = os.path.split(path)
+	if head == '':
+		return [tail] + result
+	if head == path:
+		return result
+	return fullsplit(head, [tail] + result)
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+	os.chdir(root_dir)
+
+sellmo_dir = 'sellmo'
+for dirpath, dirnames, filenames in os.walk(sellmo_dir):
+	# Ignore PEP 3147 cache dirs and those whose names start with '.'
+	for i, dirname in enumerate(dirnames):
+		if dirname.startswith('.') or dirname == '__pycache__':
+			del dirnames[i]
+	if '__init__.py' in filenames:
+		packages.append('.'.join(fullsplit(dirpath)))
+	elif filenames:
+		data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
 setup(
 	name='Sellmo',
 	version='0.1',
@@ -35,6 +69,7 @@ setup(
 	author='Raymond Reggers',
 	author_email='raymond@adaptiv.nl',
 	url='http://www.adaptiv.nl',
-	packages=find_packages(),
+	packages=packages,
+	data_files=data_files,
 	install_requires=[],
 )
