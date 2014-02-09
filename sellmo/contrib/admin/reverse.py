8326c0b802bc59b6cc6e61a1bcb5dc4045cb2cdd
Raymond Reggers <raymond@adaptiv.nl>
2014-02-09 19:24:45 +0100

    Some cleanup

--- sellmo/contrib/admin/reverse.py
+++ sellmo/contrib/admin/reverse.py
@@ -1,8 +1,36 @@
+# Copyright (c) 2012, Adaptiv Design
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#    * Neither the name of the <ORGANIZATION> nor the names of its contributors may
+# be used to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+#
+
+from django.db import transaction, models
+from django.db.models import OneToOneField, ForeignKey
 from django.contrib.admin import helpers, ModelAdmin
 from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.admin.util import flatten_fieldsets
-from django.db import transaction, models
-from django.db.models import OneToOneField, ForeignKey
 from django.forms import ModelForm
 from django.forms.formsets import all_valid
 from django.forms.models import BaseModelFormSet, modelformset_factory
@@ -12,6 +40,8 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 from django.core.exceptions import PermissionDenied
 
+#
+
 class ReverseInlineFormSet(BaseModelFormSet):
 	model = None
 	parent_fk_name = None
@@ -20,36 +50,35 @@ class ReverseInlineFormSet(BaseModelFormSet):
 			obj = getattr(instance, self.parent_fk_name)
 		except self.model.DoesNotExist:
 			obj = None
-		
+			
 		if obj:
 			queryset = self.model.objects.filter(pk=obj.id)
 		else:
 			queryset = self.model.objects.none()
+		
 		if queryset.count() == 0:
 			self.extra = 1
 		super(ReverseInlineFormSet, self).__init__(data, files, prefix=prefix, queryset=queryset)
 
 def reverse_inlineformset_factory(parent_model, model, parent_fk_name, form=ModelForm, fields=None, exclude=None, formfield_callback=lambda f:f.formfield()):
 	kwargs = {
-		'form': form,
-		'formfield_callback': formfield_callback,
-		'formset': ReverseInlineFormSet,
-		'extra': 0,
-		'can_delete': False,
-		'can_order': False,
-		'fields': fields,
-		'exclude': exclude,
-		'max_num': 1,
+		'form' : form,
+		'formfield_callback' : formfield_callback,
+		'formset' : ReverseInlineFormSet,
+		'extra' : 0,
+		'can_delete' : False,
+		'can_order' : False,
+		'fields' : fields,
+		'exclude' : exclude,
+		'max_num' : 1,
 	}
+	
 	FormSet = modelformset_factory(model, **kwargs)
 	FormSet.parent_fk_name = parent_fk_name
 	return FormSet
 
 class ReverseInlineModelAdmin(InlineModelAdmin):
-	'''
-	Use the name and the help_text of the owning models field to
-	render the verbose_name and verbose_name_plural texts.
-	'''
+	
 	def __init__(self, parent_model, parent_fk_name, model, admin_site, inline_type):
 		self.template = 'admin/edit_inline/%s.html' % inline_type
 		self.parent_fk_name = parent_fk_name
